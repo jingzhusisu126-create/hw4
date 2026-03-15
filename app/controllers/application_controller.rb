31cf425b74eb32b01fdf0b96ec0b8094cc0cfded
jingzhusisu126-create <jingzhu.sisu126@gmail.com>
2026-03-15 23:05:46 +0000

    success

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   def authenticate_user
     unless session["user_id"]
-      redirect_to "/sessions/new"
+      redirect_to "/login"
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
     @user = User.new
     @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user.password = params["password"]
     @user.save
-    redirect_to "/"
+    session["user_id"] = @user.id
+    flash["notice"] = "Thanks for signing up!"
+    redirect_to "/places"
   end
 end
