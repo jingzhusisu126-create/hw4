d927c13c411c61d3fd615bbb6be15f34d0230203
jingzhusisu126-create <jingzhu.sisu126@gmail.com>
2026-03-15 21:59:18 +0000

    1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,26 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    # 核心得分点：检查用户是否登录
+    @user = User.find_by({ "id" => session["user_id"] })
+    
+    if @user
+      @places = Place.all
+    else
+      # 如果没登录，直接踢回登录页面或显示提示
+      flash["notice"] = "Please login first."
+      redirect_to "/sessions/new"
+    end
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user
+      @place = Place.find_by({ "id" => params["id"] })
+      @entries = Entry.where({ "place_id" => @place["id"] })
+    else
+      redirect_to "/sessions/new"
+    end
   end
 
   def new
