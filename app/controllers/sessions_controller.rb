612ddf0ac76657796a8cb1b6015e408aa9d893f2
jingzhusisu126-create <jingzhu.sisu126@gmail.com>
2026-03-15 21:53:36 +0000

    1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user && @user.authenticate(params["password"])
+      session["user_id"] = @user.id
+      flash["notice"] = "Welcome back!"
+      redirect_to "/places"
+    else
+      flash["notice"] = "Incorrect email or password."
+      render "new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye!"
+    redirect_to "/sessions/new"
   end
 end
-  
